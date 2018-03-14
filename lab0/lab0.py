333f9c17c2c7c0d6b275b75c8417ca91de4e746c
rcchen0526 <34439329+rcchen0526@users.noreply.github.com>
2018-03-14 23:06:41 +0800

    Update lab0.py

--- lab0/lab0.py
+++ lab0/lab0.py
@@ -6,3 +6,19 @@ class Neural:
 		self.node = 3
 		self.w0 = [random.random() for x in range(self.node)]
 		self.w1 = [random.random() for x in range(self.node)]
+
+		def main():
+	pat = [
+		[[0,0,0], [0]],
+		[[0,0,1], [1]],
+		[[0,1,0], [1]],
+		[[1,0,0], [1]],
+		[[0,1,1], [0]],
+		[[1,0,1], [0]],
+		[[1,1,0], [0]],
+		[[1,1,1], [1]]
+	]
+	NN = Neural(pat)
+	print(NN.w0, NN.w1)
+if __name__ == "__main__":
+	main()
