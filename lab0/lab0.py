55cef5c94bd5522e03bd5fe256456f881ebe5dc9
rcchen0526 <34439329+rcchen0526@users.noreply.github.com>
2018-03-16 23:11:29 +0800

    Update lab0.py

--- lab0/lab0.py
+++ lab0/lab0.py
@@ -6,61 +6,52 @@ class Neural:
 		self.input_size = 3#contain a bias
 		self.node = 3
 		#self.output_size = 1
-		self.step = 40000
+		self.step = 2000
 		self.lr0, self.lr1 = 0.5, 1
-		self.lr = 1
+		self.lr = 1.5
 		self.w0 = []
-		self.w1 = [random.uniform(-1, 1) for x in range(self.node)]
+		self.w1 = [random.uniform(-1., 1.) for x in range(self.node)]
 
 		for i in range(self.input_size):
-			self.w0.append([random.uniform(-1, 1) for x in range(self.node)])
-			
-		self.change_input = [[0.0] * self.node for x in range(self.input_size)]
-		self.change_out = [0.0 for x in range(self.node)]
+			self.w0.append([random.uniform(-1., 1.) for x in range(self.node)])
+
+		self.w0 = np.array(self.w0)
+		self.w1 = np.array(self.w1)
+		self.change_input = np.array([[0.0] * self.node for x in range(self.input_size)])
+		self.change_out = np.array([0.0 for x in range(self.node)])
 
 	def run(self, input):
-		self.ai, self.ah, self.ao = [], [], 0.0
-		
-		for i in range(self.input_size - 1):
-			self.ai.append(input[i])
-		self.ai.append(1)
-
-		for i in range(self.node):
-			sum = 0
-			for j in range(self.input_size):
-				sum += self.w0[j][i] * self.ai[j]
-			self.ah.append(sigmoid(sum))
-
-		sum = 0
-		for j in range(self.node):
-			sum += self.w1[j] * self.ah[j]
-		self.ao = sigmoid(sum)
+		self.ai = np.array([1,1,1])
+		self.ai[0] = input[0]
+		self.ai[1] = input[1]
+		self.ah = sigmoid(self.w0.T.dot(self.ai))
+		self.ao = sigmoid(self.w1.T.dot(self.ah))
 		return self.ao
 
 	def back_propagation(self, error):
 		out_delta = (error) * dsigmoid(self.ao)
-		for i in range(self.node):
-			delta_weight = self.ah[i] * out_delta
-			self.w1[i] += self.lr0*self.change_out[i] + self.lr1*delta_weight
-			self.change_out[i]=delta_weight
-
-		delta_hidden = [0.0 for x in range(self.node)]
-		for i in range(self.node):
-			error = 0.0
-			error += self.w1[i] * out_delta
-			delta_hidden[i]= error * dsigmoid(self.ah[i])
-
+		
+		delta_weight = self.ah * out_delta 
+		self.w1 += self.lr0 * self.change_out + self.lr1 * delta_weight
+		self.change_out = delta_weight
+			
+		delta_hidden = np.array([0.0 for x in range(self.node)])
+		
+		delta_hidden = self.w1 * out_delta * dsigmoid(self.ah)
+		
 		for i in range(self.input_size):
-			for j in range(self.node):
-				delta_input = self.ai[i] * delta_hidden[j]
-				self.w0[i][j] += self.lr0*self.change_input[i][j] + self.lr1*delta_input
-				self.change_input[i][j]=delta_input
+			delta_input = self.ai[i] * delta_hidden
+			self.w0[i] += 	self.lr0 * self.change_input[i] + self.lr1 * delta_input
+			self.change_input[i] = delta_input
 
 	def train(self, pattern):
 		for i in range(self.step):
+			x = 0
 			for p in pattern:
 				error = p[1] - self.run(p[0])
 				self.back_propagation(error)
+				x += np.abs(error)
+			#print(x)
 	def test(self, pattern):
 		for p in pattern:
 			print(p, self.run(p))
@@ -75,9 +66,9 @@ def dsigmoid(x):
 def main():
 	pat = (
 		[[0,0], [0]],
-		[[0,1], [1]],
-		[[1,0], [1]],
 		[[1,1], [0]],
+		[[1,0], [1]],
+		[[0,1], [1]],
 		)
 	
 	NN = Neural()
